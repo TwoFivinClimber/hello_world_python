05b6797761e9c5d387edf541137e05aa1c1e29c5
Brett Hughes <twofivinclimber@gmail.com>
2022-10-10 21:13:58 -0500

    hello world project done

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+
+animals = [ "Triceratops", "Gorilla", "Corgi", "Toucan"]
+
+for animal in animals:
+    if len(animal) >= 5:
+      print(animal)  
+
+
+user_greeting = input("What greeting would you like? Southern, Eastern, Western").lower()
+
+if user_greeting == "southern":
+  print("Howdy y\'all")
+elif user_greeting == "eastern":
+  print("Hey, How ya doin\'")
+elif user_greeting == "western":
+  print("Chea, what up braaaaah")
+else:
+  print("Howdy y\'all")
+  
